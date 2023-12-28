9a88446a84a8cc093f7e4ed163f0c471e195977d
carina-web-me <145882084+carina-web-me@users.noreply.github.com>
2023-12-28 15:15:52 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,17 @@
+//変数の宣言
+let bodyWeight;
+
+//値の代入
+bodyWeight = 68;
+
+//変数の宣言
+let bodyHeight;
+
+//値の代入
+bodyHeight = 1.7;
+
+//四則計算
+let Bmi = bodyWeight / (bodyHeight * bodyHeight);
+
+//コンソールへの出力
+console.log(Bmi);
