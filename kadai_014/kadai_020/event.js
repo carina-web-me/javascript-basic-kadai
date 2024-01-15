f5a5e323e2357dae36dc048f6317a7008d1ae4d3
carina-web-me <145882084+carina-web-me@users.noreply.github.com>
2024-01-15 12:10:57 +0900

    Add files via upload

---
+++ kadai_014/kadai_020/event.js
@@ -0,0 +1,10 @@
+//HTML要素を取得し、定数に代入する
+const btn = document.getElementById(`btn`);
+const text = document.getElementById(`text`);
+
+//HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener(`click`,() => {
+  //クリックされたら`ボタンをクリックしました`と表示
+  text.textContent = `ボタンをクリックしました`;
+
+})
